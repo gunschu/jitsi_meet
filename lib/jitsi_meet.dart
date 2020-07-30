d2cb6c63e9ebecbf42da1d6dc285165263151f23
sidrao2006 <66567899+sidrao2006@users.noreply.github.com>
2020-07-30 15:53:19 +0530

    Formated jitsi_meet.dart file

--- lib/jitsi_meet.dart
+++ lib/jitsi_meet.dart
@@ -19,19 +19,20 @@ class JitsiMeet {
   static Map<String, JitsiMeetingListener> _perMeetingListeners = {};
   static bool _hasInitialized = false;
 
-  static final Map<RoomNameConstraintType, RoomNameConstraint> defaultRoomNameConstraints =
-  {
-    RoomNameConstraintType.MIN_LENGTH : new RoomNameConstraint(
-            (value) { return value.trim().length >= 3; },
-        "Minimum room length is 3"),
+  static final Map<RoomNameConstraintType, RoomNameConstraint>
+      defaultRoomNameConstraints = {
+    RoomNameConstraintType.MIN_LENGTH: new RoomNameConstraint((value) {
+      return value.trim().length >= 3;
+    }, "Minimum room length is 3"),
 
 //    RoomNameConstraintType.MAX_LENGTH : new RoomNameConstraint(
 //            (value) { return value.trim().length <= 50; },
 //            "Maximum room length is 50"),
 
-    RoomNameConstraintType.ALLOWED_CHARS : new RoomNameConstraint(
-            (value) { return RegExp(r"^[a-zA-Z0-9-_]+$", caseSensitive: false, multiLine: false).hasMatch(value); },
-        "Only alphanumeric, dash, and underscore chars allowed"),
+    RoomNameConstraintType.ALLOWED_CHARS: new RoomNameConstraint((value) {
+      return RegExp(r"^[a-zA-Z0-9-_]+$", caseSensitive: false, multiLine: false)
+          .hasMatch(value);
+    }, "Only alphanumeric, dash, and underscore chars allowed"),
 
 //    RoomNameConstraintType.FORBIDDEN_CHARS : new RoomNameConstraint(
 //            (value) { return RegExp(r"[\\\/]+", caseSensitive: false, multiLine: false).hasMatch(value) == false; },
@@ -42,25 +43,25 @@ class JitsiMeet {
   /// A JitsiMeetingListener can be attached to this meeting that will automatically
   /// be removed when the meeting has ended
   static Future<JitsiMeetingResponse> joinMeeting(JitsiMeetingOptions options,
-      {JitsiMeetingListener listener, Map<RoomNameConstraintType, RoomNameConstraint> roomNameConstraints}) async {
+      {JitsiMeetingListener listener,
+      Map<RoomNameConstraintType, RoomNameConstraint>
+          roomNameConstraints}) async {
     assert(options != null, "options are null");
     assert(options.room != null, "room is null");
     assert(options.room.trim().isNotEmpty, "room is empty");
 
     // If no constraints given, take default ones
     // (To avoid using constraint, just give an empty Map)
-    if(roomNameConstraints == null)
-    {
+    if (roomNameConstraints == null) {
       roomNameConstraints = defaultRoomNameConstraints;
     }
 
     // Check each constraint, if it exist
     // (To avoid using constraint, just give an empty Map)
-    if(roomNameConstraints.isNotEmpty)
-    {
-      for(RoomNameConstraint constraint in roomNameConstraints.values)
-      {
-        assert(constraint.checkConstraint(options.room), constraint.getMessage());
+    if (roomNameConstraints.isNotEmpty) {
+      for (RoomNameConstraint constraint in roomNameConstraints.values) {
+        assert(
+            constraint.checkConstraint(options.room), constraint.getMessage());
       }
     }
 
@@ -216,8 +217,7 @@ class JitsiMeetingResponse {
   }
 }
 
-class JitsiMeetingOptions
-{
+class JitsiMeetingOptions {
   String room;
   String serverURL;
   String subject;
@@ -233,12 +233,10 @@ class JitsiMeetingOptions
 
   /// Get feature flags Map with keys as String instead of Enum
   /// Useful as an argument sent to the Kotlin/Swift code
-  Map<String, bool> getFeatureFlags()
-  {
+  Map<String, bool> getFeatureFlags() {
     Map<String, bool> featureFlagsWithStrings = new HashMap();
 
-    featureFlags.forEach((key, value)
-    {
+    featureFlags.forEach((key, value) {
       featureFlagsWithStrings[FeatureFlagHelper.featureFlags[key]] = value;
     });
 
@@ -246,8 +244,7 @@ class JitsiMeetingOptions
   }
 
   @override
-  String toString()
-  {
+  String toString() {
     return 'JitsiMeetingOptions{room: $room, serverURL: $serverURL, subject: $subject, token: $token, audioMuted: $audioMuted, audioOnly: $audioOnly, videoMuted: $videoMuted, userDisplayName: $userDisplayName, userEmail: $userEmail, iosAppBarRGBAColor :$iosAppBarRGBAColor, featureFlags: $featureFlags }';
   }
 
