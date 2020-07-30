b72ab3de44f3247efe12aa9b7117eb879c6b3a72
sidrao2006 <66567899+sidrao2006@users.noreply.github.com>
2020-07-30 15:55:18 +0530

    Formated room_name_constraints.dart file

--- lib/room_name_constraint.dart
+++ lib/room_name_constraint.dart
@@ -2,7 +2,24 @@
 class RoomNameConstraint
 {
   Function(String) _checkFunction;
+  String _constraintMessage;class RoomNameConstraint {
+  Function(String) _checkFunction;
   String _constraintMessage;
+
+  RoomNameConstraint(Function(String) checkFunction, String constraintMessage) {
+    _checkFunction = checkFunction;
+    _constraintMessage = constraintMessage;
+  }
+
+  bool checkConstraint(String value) {
+    return _checkFunction(value);
+  }
+
+  String getMessage() {
+    return _constraintMessage;
+  }
+}
+
 
   RoomNameConstraint(Function(String) checkFunction, String constraintMessage)
   {
