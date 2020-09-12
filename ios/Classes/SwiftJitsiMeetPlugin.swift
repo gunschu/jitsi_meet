a5e60369214125efb47218107d8470d1f5cf5722
Jimmy Jose <jimmy@snapworks.me>
2020-09-12 15:48:40 +0530

    Leave Meeting Code added
    Leave Meeting Code added

--- ios/Classes/SwiftJitsiMeetPlugin.swift
+++ ios/Classes/SwiftJitsiMeetPlugin.swift
@@ -114,6 +114,7 @@ public class SwiftJitsiMeetPlugin: NSObject, FlutterPlugin, FlutterStreamHandler
             
             dictClosingServerInfo["url"] = "\(serverURL)/\(roomName)";
             
+            self.jitsiViewController?.closeJitsiMeeting();
             self.jitsiViewController?.conferenceTerminated(dictClosingServerInfo);
         
         
