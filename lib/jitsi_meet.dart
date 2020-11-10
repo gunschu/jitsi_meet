1bd33fdee7daf7f52687ebf5f285165ba405a079
VanQuan14 <nguyenvanquan14041997@gmail.com>
2020-11-11 03:20:58 +0700

    add userAvatarURL IOS

--- lib/jitsi_meet.dart
+++ lib/jitsi_meet.dart
@@ -98,6 +98,7 @@ class JitsiMeet {
           'featureFlags': options.getFeatureFlags(),
           'userDisplayName': options.userDisplayName,
           'userEmail': options.userEmail,
+          'userAvatarURL': options.userAvatarURL,
         })
         .then((message) =>
             JitsiMeetingResponse(isSuccess: true, message: message))
@@ -219,6 +220,7 @@ class JitsiMeetingOptions {
   bool videoMuted;
   String userDisplayName;
   String userEmail;
+  String userAvatarURL;
 
   Map<FeatureFlagEnum, bool> featureFlags = new HashMap();
 
