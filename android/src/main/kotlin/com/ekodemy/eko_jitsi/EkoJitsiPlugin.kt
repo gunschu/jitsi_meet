a667450759b4f0dcbddf4863af6c72bc8fc40031
ekodemy <shubham.ekodemy@gmail.com>
2022-11-06 00:30:21 +0530

    Fix

--- android/src/main/kotlin/com/ekodemy/eko_jitsi/EkoJitsiPlugin.kt
+++ android/src/main/kotlin/com/ekodemy/eko_jitsi/EkoJitsiPlugin.kt
@@ -78,7 +78,7 @@ public class EkoJitsiPlugin() : FlutterPlugin, MethodCallHandler, ActivityAware 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val plugin = EkoJitsiPlugin(registrar.activity())
+            val plugin = EkoJitsiPlugin(registrar.activity()!)
             val channel = MethodChannel(registrar.messenger(), JITSI_METHOD_CHANNEL)
             channel.setMethodCallHandler(plugin)
 
